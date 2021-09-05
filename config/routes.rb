60f498b6b7c3d39334b0aca56f0b9cbb71a50e5c
EC2 Default User <ec2-user@ip-172-31-71-109.ec2.internal>
2021-09-05 01:20:07 +0000

    [Start]2-6

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ post 'todolists' => 'todolists#create'
 get 'todolists' => 'todolists#index'
 # .../todolists/:1や .../todolists/3に該当する
 get 'todolists/:id' => 'todolists#show',as:'todolist'
-
+get 'todolists/:id/edit' => 'todolists#edit',as:'edit_todolist'
 end
